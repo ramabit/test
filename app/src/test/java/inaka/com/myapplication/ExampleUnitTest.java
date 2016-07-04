c7608fda5d2d14a04828e72abb55e9014d6e1ec3
ramabit <f.e.ramirez94@gmail.com>
2016-07-04 17:57:10 -0300

    test

--- app/src/test/java/inaka/com/myapplication/ExampleUnitTest.java
+++ app/src/test/java/inaka/com/myapplication/ExampleUnitTest.java
@@ -1,5 +1,8 @@
 package inaka.com.myapplication;
 
+import com.ad.android.tools.lint.Lint;
+
+import org.junit.Rule;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -8,6 +11,10 @@ import static org.junit.Assert.*;
  * To work on unit tests, switch the Test Artifact in the Build Variants view.
  */
 public class ExampleUnitTest {
+
+    @Rule public Lint lint = new Lint(new MyCustomRule(), MyCustomRule.ISSUE);
+
+
     @Test
     public void addition_isCorrect() throws Exception {
         assertEquals(4, 2 + 2);
